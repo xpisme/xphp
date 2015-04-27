5dccd00800c756fceb9d51a3fb25c8ca2211f6f5
stepen <1067154883@qq.com>
2015-04-27 21:21:02 +0800

    php基础框架

---
+++ Conf/system.php
@@ -0,0 +1,25 @@
+<?php 
+
+/**
+ * @Author:      xp
+ * @DateTime:    2015-04-18 10:06:44
+ * @Description: 系统配置文件
+ */
+
+/*数据库配置*/
+namespace Conf;
+defined('ACC')||exit('ACC Denied');
+
+$CONFIG['db'] = array(
+	'db_host' => 'localhost',
+	'db_user' => 'root',
+	'db_pass' => '',
+	'db_name' => 'test',
+	'db_char' => 'utf8',
+	'db_type' => 'pdo', // mysql mysqli pdo
+	'db_prefix'=> 'm_', // 数据表前缀
+);
+
+$CONFIG['lang'] = 'zh_cn';
+
+ ?>
---
+++ View/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>xphp</title>
+</head>
+<body>
+	<h3>Welcome xphp</h3>
+	<p>欢迎使用xphp框架</p>
+</body>
+</html>
